7f083ba9cb158943c2068158c670fb0f3559b8a1
Abdurrehman <tritechuzaifa@gmail.com>
2023-12-28 13:10:24 +0500

    Hedd

--- lib/views/shops/shop_profile/shop_profile_controller.dart
+++ lib/views/shops/shop_profile/shop_profile_controller.dart
@@ -2,13 +2,14 @@ import 'package:get/get.dart';
 import 'package:hume/api/product_api.dart';
 import 'package:hume/api/shop_api.dart';
 import 'package:hume/helper/loading.dart';
+import 'package:hume/models/product.dart';
 import 'package:hume/models/shop.dart';
 
 class ShopProfileController extends GetxController {
   final ShopApi shopApi = ShopApi();
   final ProductApi productApi = ProductApi();
   Shop? shop;
- 
+  List<Product> products = [];
 
   @override
   void onInit() {
@@ -19,6 +20,7 @@ class ShopProfileController extends GetxController {
   Future<void> fetchShopAndProducts(shopId) async {
     LoadingHelper.show();
     shop = await shopApi.getShopData(shopId);
+    products = await shopApi.fetchProductsByShopwithLimit6("1");
     update();
     LoadingHelper.dismiss();
   }
